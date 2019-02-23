2f8c7187e344ecd3d85c5c89e16fd9090f625aa5
caffi-nate <nathan@caffinate.com>
2019-02-24 03:26:37 +0800

    Added section change js filter, better projects code

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -18,6 +18,7 @@ function galleryParallax(e){
 
             let xWalk = 50;
             let yWalk = 50;
+            let scale = 1;
 
             //console.log(e.target);
             if (galleryItem.contains(e.target)){
@@ -28,6 +29,7 @@ function galleryParallax(e){
               //console.log("inside!");
               //console.log(galleryItem.offsetWidth);
               //console.log(x);
+              scale = 1.2;
               const { offsetWidth: width, offsetHeight: height } = galleryItem;
               const walkAmount = 100;
               //xWalk = 50 + (x / width * walkAmount);
@@ -42,8 +44,85 @@ function galleryParallax(e){
             //console.log(galleryBG);
 
             const backgroundPos = window.getComputedStyle(galleryBG).backgroundPosition;
-            galleryBG.style.backgroundPosition = `${xWalk}% ${yWalk}%`;
+            //galleryBG.style.backgroundPosition = `${xWalk}% ${yWalk}%`;
+            //galleryBG.style.scale = `${scale}`;
     });
 }
 
+
+function calculateSectionProximity(){
+	const sections = document.querySelectorAll('section');
+	const screen_height = window.innerHeight;
+	const window_midpoint = window.pageYOffset + screen_height * 0.5;
+	const window_top = window.pageYOffset;
+	//console.log(window_top);
+	let bodyRect = document.body.getBoundingClientRect();
+	for (i = 0; i < sections.length; i++){
+	//for (i = 0; i < 1; i++){
+		let section = sections[i];
+		let boundingBox = section.getBoundingClientRect();
+
+		let section_midPoint = (boundingBox.top-bodyRect.top + boundingBox.bottom - bodyRect.bottom) * 0.5;
+
+
+		let section_top = (boundingBox.top - bodyRect.top);
+		//console.log(boundingBox.top);
+		let distance = Math.abs(window_midpoint - section_midPoint);
+		distance = Math.abs(window_top - section_top);
+		//console.log(distance);
+		//console.log(distance);
+		//console.log(distance);
+		// long way of clamping I guess
+		let distance_adjusted = distance * 0.5;
+		if (distance_adjusted > 100) distance_adjusted = 100;
+		else if (distance_adjusted < 0) distance_adjusted = 0;
+
+		distance_adjusted = 100;
+		if (distance < (screen_height*0.5)){
+			//console.log("Within proximity!" + i);
+
+			distance_adjusted = 0; // test
+			//console.log(distance);
+			//`grayscale(${distance});`;
+			//filter: grayscale(100%);
+		}
+		//section.style.filter = `grayscale(${distance_adjusted}%) opacity(${1 - 0.01 * distance_adjusted})`;
+		//section.style.filter = `grayscale(${distance_adjusted}%)`;
+		//section.style.filter = `sepia(${distance_adjusted}%)`;
+		section.style.filter = `saturate(${100 - 0.9*distance_adjusted}%)`;
+
+		// for each child, apply a blur filter
+		// var children = section.children;
+		// console.log(children);
+		//galleryItems.forEach (galleryItem => {
+		// for (i = 0; i < children.length; i++){
+		//
+		//
+		// //children.forEach(child =>{
+		// 	//console.log("is child");
+		// 	children[i].style.filter = `blur(${4 * distance_adjusted}px)`;
+		// }
+		//filter = `blur(${4 * distance_adjusted}px)`;
+
+		//console.log(Math.abs(midPoint - window_ypos));
+		//console.log(boundingBox.top);
+		//const section_ypos =
+		//console.log("hello!");
+	}
+}
+
+
+
+
+
+function loadProjectBackgrounds(){
+    const galleryItems = document.querySelectorAll('#projects > ul > li > .project-background');
+    for (i = 0; i < galleryItems.length; i++){
+        galleryItems[i].style.backgroundImage = (`url("/images/web-project-${i}.png")`);
+    }
+}
+
+
+loadProjectBackgrounds();
 window.addEventListener('mousemove', galleryParallax);
+window.addEventListener('scroll', calculateSectionProximity)
