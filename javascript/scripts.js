347f5b73206e7b91203d0a41efb2eb69c4b2030c
caffi-nate <nathan@caffinate.com>
2019-02-23 21:38:58 +0800

    projects gallery update with javascript hover effects

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,3 +4,46 @@ function shake(){
 		icon.classList.add('shake');
 	});
 }
+
+
+
+
+/// parallax gallery images according to mouse movement
+function galleryParallax(e){
+    let { offsetX: x, offsetY: y } = e;
+    const galleryItems = document.querySelectorAll('#projects > ul > li');
+    //console.log(galleryItems);
+    galleryItems.forEach (galleryItem => {
+            //console.log(e.target);
+
+            let xWalk = 50;
+            let yWalk = 50;
+
+            //console.log(e.target);
+            if (galleryItem.contains(e.target)){
+            //if (e.target == galleryItem) {
+                //console.log(e.target);
+              // x = x + e.target.offsetLeft;
+              // y = y + e.target.offsetTop;
+              //console.log("inside!");
+              //console.log(galleryItem.offsetWidth);
+              //console.log(x);
+              const { offsetWidth: width, offsetHeight: height } = galleryItem;
+              const walkAmount = 100;
+              //xWalk = 50 + (x / width * walkAmount);
+              yWalk = 50 - (y / height * walkAmount);
+
+
+              // add hover effect here instead
+            }
+
+
+            const galleryBG = galleryItem.firstElementChild;
+            //console.log(galleryBG);
+
+            const backgroundPos = window.getComputedStyle(galleryBG).backgroundPosition;
+            galleryBG.style.backgroundPosition = `${xWalk}% ${yWalk}%`;
+    });
+}
+
+window.addEventListener('mousemove', galleryParallax);
