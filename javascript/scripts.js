6095b44a38809a83c6521d6339c0fa08f59c2fc4
caffi-nate <nathan@caffinate.com>
2019-03-01 20:52:30 +0800

    SCSS Cleanup, colour palette tests, minor fixes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,7 +2,7 @@ function loadProjectBackgrounds(){
     //const galleryItems = document.querySelectorAll('#projects > ul > li > .project-background');
 	const galleryItems = document.querySelectorAll('.project > a > .project-background');
     for (i = 0; i < galleryItems.length; i++){
-        galleryItems[i].style.backgroundImage = (`url("/images/web-project-${i}.png")`);
+        galleryItems[i].style.backgroundImage = (`url("images/web-project-${i}.png")`);
     }
 }
 
